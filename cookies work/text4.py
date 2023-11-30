fc534d5a18cb3095fb99b268f96561ffc851864c
Firoz Rahman Sabuz <everythingkst@gmail.com>
2023-11-30 16:50:55 +0600

    file added

--- cookies work/text4.py
+++
@@ -1,6 +0,0 @@
-import pickle
-import selenium.webdriver
-
-driver = selenium.webdriver.Firefox()
-driver.get("https://elements.envato.com/")
-pickle.dump(driver.get_cookies(), open("cookies.pkl", "wb"))
